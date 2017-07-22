eb4520251786f107c08b4afb21eefee7b3f45de2
Cody Tacderas <codytacderas@Codys-MacBook-Air.local>
2017-07-21 18:21:36 -0600

    With Spotify issues

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ck3NmfD7LpUOBhbc6YrkU9J0W',
+  consumer_secret: 'MkExcQ9soo9ICVqlCrKubSm4daciOrK8sWw755R8zqBfnxHo8W',
+  access_token_key: '888523472116236288-KwmFgODvDS6aFIbklhzDLXFCEEZPXEP',
+  access_token_secret: 'o8Jir0Mwcq79rRQDQlXEAR2XxlrUr643bpOmQ5B28tqNA',
+}
+
+exports.spotifyKeys = {
+	id: '49fd9fa7ff8949b6a9e8549bf121be0b',
+	secret: '5b13b662c05b41e2bc9b12f34739bbd9',
+};
